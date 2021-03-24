5d2a0039d7df929b825f817676c0001a3cda6022
Stone <stone.dummett@gmail.com>
2021-03-24 11:21:53 +0100

    corrected ft_memcmp, it returns 0 when size = 0

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 13:00:24 by sdummett          #+#    #+#             */
-/*   Updated: 2021/03/23 16:17:19 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/03/24 11:17:59 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 int	ft_memcmp(const void *b1, const void *b2, size_t len)
 {
-	while (--len)
+	if (len)
 	{
-		if (*(unsigned char *)b1 != *(unsigned char *)b2)
-			return (*(unsigned char *)b1 - *(unsigned char *)b2);
-		b1++;
-		b2++;
+		while (--len)
+		{
+			if (*(unsigned char *)b1 != *(unsigned char *)b2)
+				return (*(unsigned char *)b1 - *(unsigned char *)b2);
+			b1++;
+			b2++;
+		}
+		return (*(unsigned char *)b1 - *(unsigned char *)b2);
 	}
-	return (*(unsigned char *)b1 - *(unsigned char *)b2);
+	else
+		return (0);
 }
