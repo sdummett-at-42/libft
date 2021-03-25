54b63bcc45ca7cb60ed5f30e0c689ff33f38b324
Stone <stone.dummett@gmail.com>
2021-03-25 13:29:02 +0100

    ft_strchr / ft_strrchr : added a cast in (unsigned char) for ch in order to compare it with a char

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 22:18:37 by sdummett          #+#    #+#             */
-/*   Updated: 2021/03/23 15:56:45 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/03/25 13:14:15 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*ft_strchr(const char *str, int ch)
 {
 	while (*str)
 	{
-		if (*str == ch)
+		if (*str == (unsigned char)ch)
 			return ((char *)str);
 		str++;
 	}
-	if (*str == ch)
+	if (*str == (unsigned char)ch)
 		return ((char *)str);
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 22:33:37 by sdummett          #+#    #+#             */
-/*   Updated: 2021/03/23 15:59:36 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/03/25 13:16:09 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strrchr(const char *str, int ch)
 	ptr = 0;
 	while (*str)
 	{
-		if (*str == ch)
+		if (*str == (unsigned char)ch)
 			ptr = str;
 		str++;
 	}
-	if (*str == ch)
+	if (*str == (unsigned char)ch)
 		return ((char *)str);
 	return ((char *)ptr);
 }
