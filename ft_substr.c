47a3a685efcea7d28138da834b5b8ea53ec0be5c
Stone <stone.dummett@gmail.com>
2021-03-25 09:50:29 +0100

    ft_substr corrected an error, when the strlen of s - start was smaller than len, the wrong size was mallocd

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 14:45:28 by sdummett          #+#    #+#             */
-/*   Updated: 2021/03/23 16:00:14 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/03/25 09:46:06 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 
-	ptr = malloc(sizeof(char) * len + 1);
-	if (!ptr)
+	if (!s)
 		return (0);
-	*(ptr + len) = 0;
 	if (!(start > ft_strlen(s)))
 	{
+		if (ft_strlen(s) - start < len)
+			len = ft_strlen(s) - start;
+		ptr = malloc(sizeof(char) * len + 1);
+		if (!ptr)
+			return (0);
+		*(ptr + len) = 0;
 		while (len)
 		{
 			*(ptr + len - 1) = *(s + start + len - 1);
 			len--;
 		}
+		return (ptr);
 	}
 	else
-		while (len--)
-			*(ptr + len - 1) = 0;
-	return (ptr);
+		return (calloc(1, sizeof(char)));
 }
